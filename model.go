90538d8dc533d0490d494aed1761602807425847
GoodOBag <goodobag@gmail.com>
2016-02-28 12:15:47 -0800

    [Develop]model initial design without debug

--- model.go
+++ model.go
@@ -68,7 +68,7 @@ func itemtableReplace(items []string, units []string, unitprice []float64, notes
 	defer stmt.Close()
 	for i, _ := range items {
 		_, err = stmt.Exec(items[i], units[i], unitprice[i], notes[i])
-		checkError(err, "model-unittableReplace-3")
+		checkError(err, "model-itemtableReplace-3")
 	}
 	return true
 }
@@ -92,56 +92,154 @@ func bldgtableGet() (regdates []int, bldgs []string, addrs []string, zips []int,
 	return
 }
 
-func bldgtableReplace() bool {
+func bldgtableReplace(regdates []int, bldgs []string, addrs []string, zips []int, notes []string) bool {
+	_, err := db.Exec("delete from bldgtable")
+	checkError(err, "model-bldgtableReplace-1")
+	
+	stmt, err := db.Prepare("insert into bldgtable(regdate, bldg, addr, zip, notes) values(?,?,?,?,?)")
+	checkError(err, "model-bldgtableReplace-2")
+	defer stmt.Close()
+	for i, _ := range regdates {
+		_, err = stmt.Exec(regdates[i], bldgs[i], addrs[i], zips[i] notes[i])
+		checkError(err, "model-bldgtableReplace-3")
+	}
 	return true
 }
 
 //customer
-func customertableGet() (id []int, regdates []int, nicknames []string, phones []int64, bldg []string, room []string, notes []string) {
-
+func customertableGet() (ids []int, regdates []int, nicknames []string, phones []int64, bldgs []string, rooms []string, notes []string) {
+	rows, err := db.Query("SELECT * FROM customertable")
+	checkError(err, "model-customertableGet-1")
+	
+	var id, regdate int
+	var phone int64
+	var nickname, bldg, room, note string
+	for rows.NEXT() {
+		err = rows.Scan(&id,&regdate,&nickname,&phone,&bldg,&note)
+		checkError(err, "model-customertableGet-2")
+		ids = append(ids, id)
+		regdates = append(regdates, regdate)
+		nicknames = append(nicknames,nickname)
+		phones = append(phones, phone)
+		bldgs = append(bldgs, bldg)
+		notes = append(notes, note)
+	}
+	return
 }
 
 func customertableAppend(regdate int, nickname string, phone int64, bldg string, room string, note string) bool {
-
+	stmt, err := db.Prepare("INSERT INTO customertable(regdate, nickname, phone, bldg, room, notes) values(?,?,?,?,?,?)")
+	checkError(err, "model-customertableAppend-1")
+	
+	_, err = stmt.Exec(regdate, nickname, phone, bldg, room, note)
+    checkError(err, "model-customertableAppend-2")
+	return true
 }
 
 func customertableUpdate(id int, regdate int, nickname string, phone int64, bldg string, room string, note string) bool {
+	stmt, err = db.Prepare("update customertable set (regdate, nickname, phone, bldg, room, note)=(?,?,?,?,?,?)  where id=?")
+    checkError(err, "model-customertableUpdate-1")
 
+    res, err = stmt.Exec(regdate, nickname, phone, bldg, room, note, id)
+    checkError(err, "model-customertableUpdate-1")
+	return true
 }
 
 func customertableDelete(id int) bool(){
-	
+	stmt, err = db.Prepare("delete from customertable where id=?")
+    checkError(err, "model-customertableDelete-1")
+
+    res, err = stmt.Exec(id)
+    checkError(err, "model-customertableDelete-2")
+	return true
 }
 
 //order
-func ordertableGetDay()(){
+func ordertableGetDay(orderdate int)(ids []int, nicknames []string, orderlists []string){
+	rows, err := db.Query("SELECT * FROM ordertable")
+	checkError(err, "model-ordertableGetDay-1")
 	
+	var id, tempdate int
+	var nickname, orderlist string
+	for rows.NEXT() {
+		err = rows.Scan(&id,&nickname,&tempdate,&orderlist)
+		checkError(err, "model-ordertableGetDay-2")
+		if tempdate == orderdate{
+			ids = append(ids, id)
+			nicknames = append(nicknames,nickname)
+			orderlists = append(orderlists,orderlist)
+		}
+	}
+	return
 }
 
-func ordertableAppend() bool{
+func ordertableAppend(nickname string, orderdate int, orderlist string) bool{
+	stmt, err := db.Prepare("INSERT INTO ordertable(nickname, orderdate, orderlist) values(?,?,?)")
+	checkError(err, "model-ordertableAppend-1")
 	
+	_, err = stmt.Exec(nickname, orderdate, orderlist)
+    checkError(err, "model-ordertableAppend-2")
+	return true
 }
 
-func ordertableUpdate() bool{
-	
+func ordertableUpdate(id int, nickname string, orderdate int, orderlist string) bool{
+	stmt, err = db.Prepare("update ordertable set (nickname, orderdate, orderlist)=(?,?,?)  where id=?")
+    checkError(err, "model-ordertableUpdate-1")
+
+    res, err = stmt.Exec(nickname, orderdate, orderlist, id)
+    checkError(err, "model-ordertableUpdate-2")
+	return true
 }
 
-func ordertableDelete() bool{
-	
+func ordertableDelete(id int) bool{
+	stmt, err = db.Prepare("delete from ordertable where id=?")
+    checkError(err, "model-ordertableDelete-1")
+
+    res, err = stmt.Exec(id)
+    checkError(err, "model-ordertableDelete-2")
+	return true
 }
 
 //purchase
-func purchasetableGetDay()(){
+func purchasetableGetDay(purchasedate int)(ids []int, purchaselists []string){
+	rows, err := db.Query("SELECT * FROM purchasetable")
+	checkError(err, "model-purchasetableGetDay-1")
 	
+	var id, tempdate int
+	var purchaselist string
+	for rows.NEXT() {
+		err = rows.Scan(&id,&tempdate,&purchaselist)
+		checkError(err, "model-purchasetableGetDay-2")
+		if tempdate == purchasedate{
+			ids = append(ids, id)
+			purchaselists = append(purchaselists, purchaselist)
+		}
+	}
+	return
 }
-func purchasetableAppend() bool{
+func purchasetableAppend(purchasedate int, purchaselist string) bool{
+	stmt, err := db.Prepare("INSERT INTO purchasetable(purchasedate, purchaselist) values(?,?)")
+	checkError(err, "model-purchasetableAppend-1")
 	
+	_, err = stmt.Exec(purchasedate, purchaselist)
+    checkError(err, "model-purchasetableAppend-2")
+	return true
 }
 
-func purchasetableUpdate() bool{
-	
+func purchasetableUpdate(id int, purchasedate int, purchaselist string) bool{
+	stmt, err = db.Prepare("update purchasetable set (purchasedate, purchaselist)=(?,?)  where id=?")
+    checkError(err, "model-purchasetableUpdate-1")
+
+    res, err = stmt.Exec(purchasedate, purchaselist, id)
+    checkError(err, "model-purchasetableUpdate-2")
+	return true
 }
 
-func purchasetableDelete() bool{
-	
+func purchasetableDelete(id int) bool{
+	stmt, err = db.Prepare("delete from purchasetable where id=?")
+    checkError(err, "model-purchasetableDelete-1")
+
+    res, err = stmt.Exec(id)
+    checkError(err, "model-purchasetableDelete-2")
+	return true
 }
